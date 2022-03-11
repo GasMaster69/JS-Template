5c36a58b3ca3db60b07ffaff4db9841a5717d6ad
GasMaster69 <philip.bergling@skola.taby.se>
2022-03-11 11:12:04 +0100

    Merge branch 'master' of github.com:GasMaster69/JS-Template

--- script-1.js
+++ script-1.js
@@ -1,9 +1,19 @@
+
+
 // Översätter bildnamn till bilnamn
 const carImages = {
   "img-1.png": "Volvo",
   "img-2.png": "Tesla",
   "img-3.png": "Volkswagen",
+  "img-4.png": "Blixten",
 };
+
+const carInfo = {
+  'Volvo': 'En sketen bil hittad på skroten. Köparen fick betalt 1000 kr för att ta med sig skiten hem.',
+  'Tesla': 'Man kan tro att denna bilen är eldriven, men tittar du riktigt noga så ser du dieselgeneratorn som står i bakluckan.',
+  'Volkswagen': 'The Peoples Car, byggd av vedervärdiga människor,åt ännu sämre människor.',
+  'Blixten': 'När du undrar var din mamma är, då är Blixten inte långt ifrån henne'
+}
 
 // Taggarna output-text och output-container återfinns i HTML-filen
 let outputContainer = document.getElementById("text-output-container");
@@ -11,13 +21,16 @@ let outputText = document.getElementById("output-text");
 let carImage = document.getElementById("image-output-container");
 let title = (document.title = "JavaScript FTW!");
 let btn1 = document.getElementById("button-1");
+let btn2 = document.getElementById("button-2");
+let btn3 = document.getElementById("button-3");
+let btn4 = document.getElementById("button-4");
 
 // Man kan t ex ställa färgen på ett område
 outputContainer.style.backgroundColor = "None";
 
 // Informationstext i den vänstra rutan
 let infoText = "Möjliga bilar:<br><br>";
-let brandArray = ["Volvo", "Tesla", "Volkswagen"];
+let brandArray = ["Volvo", "Tesla", "Volkswagen", "Blixten"];
 
 for (let i = 0; i < brandArray.length; i++) {
   infoText += `Bil #${i + 1}: ${brandArray[i]}` + "<br>";
@@ -29,7 +42,16 @@ infoText += "<br>Klicka i rutan för att dra en slumpad bil!";
 outputContainer.innerHTML = infoText;
 
 // Knappraden
-btn1.textContent = "Bil #1";
+btn1.textContent = "Volvo";
+btn2.textContent = "Tesla";
+btn3.textContent = "Volkswagen";
+btn4.textContent = "Blixten";
+
+
+
+function specs() {
+  infotext =
+}
 
 // Funktion som slumpar fram en ny bild
 function newCar() {
@@ -44,11 +66,9 @@ function newCar() {
   carImage.innerHTML += `<p>${carImages[currentImage]}</p>`;
 }
 
-<<<<<<< HEAD
-// Här sker själva utmatningen
-outputContainer.innerHTML = myString;
-outputContainer.style.backgroundColor = "yellow"
-=======
+
+
 // Lägger till en EventListener till outputContainer
 outputContainer.addEventListener("click", newCar);
->>>>>>> 2eeab65c755bfbcb3f2c5d52bef88935f2759051
+
+
